a4903164c5ad2bfd11eb563875caecece987c918
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2017-01-03 14:42:20 +0100

    Processing datasets in the correct order and creating snapshots

--- generate_snapshots.pl
+++ generate_snapshots.pl
@@ -65,6 +65,12 @@ foreach my $zpool (@{$zpools})
 # sort the dataset based on the mountpoint directory level, to asure the existance of required folders
 @dataset = sort { int(@{$a->{dirs}}) <=> int(@{$b->{dirs}}) || $a->{mountpoint} cmp $b->{mountpoint} || $a->{zpool} cmp $b->{zpool} || $a->{zfs} cmp $b->{zfs} } @dataset;
 
+foreach my $current_dataset (@dataset)
+{
+    # create snapshot
+    my $snapshot_name = create_snapshot($current_dataset->{zfs}, $backup_snapshot_name);
+}
+
 # sub get_all_zpools
 #
 # returns an array of all available zpools in the system
